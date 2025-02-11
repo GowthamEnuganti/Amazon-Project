080cde05ef29641ab794dcf48b1123856828bdca
enugantigowtham@gmail.com <enugantigowtham@gamil.com>
2025-02-11 14:34:15 +0530

    Make payment summary interactive

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -7,7 +7,6 @@ export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
 
-
   cart.forEach((cartItem) => {
     const product = getProduct(cartItem.productId);
     productPriceCents += product.priceCents * cartItem.quantity;
